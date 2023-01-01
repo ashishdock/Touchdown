701e0c9ccb1aecbcc969beac7ec4d381ab5770bc
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2023-01-01 14:46:46 +0530

    Initial Commit

---
+++ Touchdown/Data/Constant.swift
@@ -0,0 +1,35 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Ashish Sharma on 01/01/2023.
+//
+
+import SwiftUI
+
+// DATA
+
+
+// COLOR
+let colorBackground: Color = Color("colorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// LAYOUT
+
+
+// UX
+
+
+// API
+
+
+// IMAGE
+
+
+// FONT
+
+
+// STRING
+
+
+// MISC
---
+++ Touchdown/View/Home/FooterView.swift
@@ -0,0 +1,38 @@
+//
+//  FooterView.swift
+//  Touchdown
+//
+//  Created by Ashish Sharma on 01/01/2023.
+//
+
+import SwiftUI
+
+struct FooterView: View {
+    var body: some View {
+        VStack(alignment: .center, spacing: 10){
+            Text("We offer the most cutting edge, comfortable, lightweight and durable footballhelmets in the market at affordable prices.")
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .layoutPriority(2)
+            
+            Image("logo-lineal")
+                .renderingMode(.template)
+                .foregroundColor(.gray)
+                .layoutPriority(0)
+            
+            Text ("Copyright © Ashish\nAll rights reserved")
+                .font(.footnote)
+                .fontWeight(.bold)
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .layoutPriority(1)
+        } //: VSTACK
+        .padding()
+    }
+}
+
+struct FooterView_Previews: PreviewProvider {
+    static var previews: some View {
+        FooterView()
+    }
+}
